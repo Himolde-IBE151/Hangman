73721ac5088b9e4139bd124629a883c0462c2ac1
HimoldeSCM1 <anmi@himolde.no>
2019-09-06 10:20:15 +0200

    Add emojis

---
+++ funnyEmojis.py
@@ -0,0 +1,20 @@
+#See more at https://www.geeksforgeeks.org/python-program-to-print-emojis/
+
+# grinning face 
+print("\U0001f600") 
+
+# grimacing face
+print("\N{grimacing face}")
+
+print("😢")
+  
+# grinning squinting face 
+print("\U0001F606") 
+  
+# rolling on the floor laughing 
+print("\U0001F923") \
+
+#face with rolling eyes
+print("\N{face with rolling eyes}") 
+
+print(u"\U0001F97A")
--- hangman1.py
+++
@@ -1,9 +0,0 @@
-# grinning face 
-print("\U0001f600") 
-  
-# grinning squinting face 
-print("\U0001F606") 
-  
-# rolling on the floor laughing 
-print("\U0001F923") 
-print(u"\U0001F97A")
